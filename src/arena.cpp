06ddd2bc73aaaf35bc3b5b3058be9bbf554b62f4
Winteractive <winteractiveSE@gmail.com>
2026-04-24 17:57:59 +0200

    Update Memory Arena functions

--- src/arena.cpp
+++ src/arena.cpp
@@ -1,5 +1,7 @@
 #include "arena.h"
 #include <cassert>
+#include <cstring>
+
 
 void Memory::Initialize(Arena* arena, void* mem_start, size_t size) {
   arena->base = (unsigned char*)mem_start;  
@@ -7,13 +9,21 @@ void Memory::Initialize(Arena* arena, void* mem_start, size_t size) {
   arena->used = 0;
 }
 
-void* Memory::Allocate(Arena *arena, size_t size) {
+void* Memory::Allocate(Arena* arena, size_t size) {
   assert(arena->used + size < arena->size);
   void* front = arena->base + arena->used;
   arena->used += size;
+  memset(front, 0, size);
   return front;
 }
 
-void Memory::Reset(Arena *arena){
+void Memory::Reset(Arena* arena){
   arena->used = 0;
+}
+
+Memory::Arena* Memory::CreateSubArena(Arena* parent_arena, size_t size){
+  Memory::Arena* sub_arena = (Memory::Arena*)Allocate(parent_arena, sizeof(Memory::Arena));
+  void* memory_start = Allocate(parent_arena, size);
+  Memory::Initialize(sub_arena, memory_start, size);
+  return sub_arena;
 }
